a94f57b88560b3dba38dfd7565d7dc72bfde27b7
Pietro1111 <pietroangelini11@gmail.com>
2020-03-18 19:22:10 +0100

    Pause Menù
    Creazione del menù pausa

---
+++ UnityTanks/Assets/Script/Pausemenù.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenù : MonoBehaviour
+{
+    //public static bool giocoinpausa = false; 
+   
+
+    // Update is called once per frame
+    public void Pausa()
+    {
+       
+            Time.timeScale =0f;
+            
+        
+    }
+    public void Riprendi()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menù");
+        
+
+    }
+}
+
+
